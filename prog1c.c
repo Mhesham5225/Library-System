5636d5aa5a9eb742b71c522ecca311e70cdc4289
Mhesham5225 <maram_hesham09@hotmail.com>
2017-12-11 00:10:50 +0200

    unfinished borrow function

--- prog1c.c
+++ prog1c.c
@@ -1,7 +1,17 @@
 #include "Prog1.h"
+#include <time.h>
 brrw Borrow()
 {
-
+    brrw b;
+    printf("Enter book ISBN: ");
+    fgets(b.ISBN,SIZE,stdin);
+    toc(b.ISBN);
+    printf("Enter user ID: ");
+    scanf("%d",&b.ID);
+    clearinput();
+    time_t curtime;
+    printf("%s",ctime(&curtime));
+    return b;
 }
 brrw Return()
 {
